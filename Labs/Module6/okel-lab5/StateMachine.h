fe61561eadd777451eff22cc5c24a6193958ec6a
okeltw <twokel.to@gmail.com>
2018-10-07 21:35:32 -0400

    Basic state machine working

--- Labs/Module6/okel-lab5/StateMachine.h
+++ Labs/Module6/okel-lab5/StateMachine.h
@@ -1,4 +1,5 @@
 #include <libmaple/libmaple.h>
+#include "RTCController.h"
 
 #define PUMP_PIN 0
 #define VENT_PIN 1
@@ -20,26 +21,37 @@ void stateMachineInit();
 void pumpOn()
 {
   digitalWrite(PUMP_PIN, HIGH);
+  uint32_t t = rtcGetCount();
+  rtcSetAlarm(t+5);
   nextState = ventOn;
 }
 void ventOn()
 {
   digitalWrite(VENT_PIN, HIGH);
+  uint32_t t = rtcGetCount();
+  rtcSetAlarm(t+5);
   nextState = pumpOff;
 }
 void pumpOff()
 {
   digitalWrite(PUMP_PIN, LOW);
+  uint32_t t = rtcGetCount();
+  rtcSetAlarm(t+5);
   nextState = ventOff;
 }
 void ventOff()
 {
   digitalWrite(VENT_PIN, LOW);
+  uint32_t currentTime = rtcGetCount();
+  uint32_t seconds = currentTime % 60;
+  uint32_t alarmTime = (currentTime-seconds) + 60;
+  rtcSetAlarm(alarmTime);
   nextState = pumpOn;
 }
 
 void stateMachineInit()
 {
-  pumpOff(); // Turn off Pump
-  ventOff(); // Turn off Vent; set handler to pumpOn
+  digitalWrite(PUMP_PIN, LOW);
+  digitalWrite(VENT_PIN, LOW);
+  nextState = pumpOn; 
 }
--- Labs/Module6/okel-lab5/okel-lab5.ino
+++ Labs/Module6/okel-lab5/okel-lab5.ino
@@ -7,57 +7,55 @@
 
 void setup() {
   Serial.begin(9600);
+  delay(1000);
+  Serial.println("Begin Revision 0.0.4");
   
   pinMode(PUMP_PIN, OUTPUT);
   pinMode(VENT_PIN, OUTPUT);
 
-  // Turn 
   stateMachineInit();
 
   rtcInit();
   rtcSetPrescaler(0x7FFF);
+
+  rtcSetCount(50);
   
   uint32_t currentTime = rtcGetCount();
   uint32_t seconds = currentTime % 60;
   uint32_t alarmTime = (currentTime-seconds) + 60;
 
-  delay(1000);
-  Serial.println("Current Time:");
-  Serial.println(currentTime);
-  Serial.println("Alarm:");
-  Serial.println(alarmTime);
+  Serial.print("Current: ");Serial.println(currentTime);
+  Serial.print("Alarm: ");Serial.println(alarmTime);
 
+  
   rtcSetAlarm(alarmTime);
-
-  rtcAttachAlarmInt(ding);
-  rtcAttachSecondInt(_printTime);
-    
-  Serial.println("Begin Revision 0.0.3.a");
+  rtcAttachAlarmInt(handler);
+  //rtcAttachSecondInt(printTime);
 }
 
 void loop() {
 }
 
-void ding()
-{
-  noInterrupts();
-  Serial.println("DING");
-  interrupts();
-}
+//void ding()
+//{
+//  noInterrupts();
+//  Serial.println("DING");
+//  interrupts();
+//}
 
-void _printTime()
+void printTime()
 {
   noInterrupts();
   Serial.println(rtcGetCount());
   interrupts();
 }
 
-void printTime(uint32_t t)
-{
-  uint32_t seconds = t % 60;
-  t /= 60;
-  uint32_t minutes = t % 60;
-
-  Serial.print(minutes); Serial.print(':');
-  Serial.println(seconds);
-}
+//void printTime(uint32_t t)
+//{
+//  uint32_t seconds = t % 60;
+//  t /= 60;
+//  uint32_t minutes = t % 60;
+//
+//  Serial.print(minutes); Serial.print(':');
+//  Serial.println(seconds);
+//}
